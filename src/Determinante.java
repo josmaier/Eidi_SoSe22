7c64e67d452b530cd2980ea80829957952477049
josch <joscha.maier@live.de>
2022-07-01 18:05:54 +0200

    added stuff, giving up

--- src/Determinante.java
+++ src/Determinante.java
@@ -1,63 +1,84 @@
 /**
- * Ein Demo-Programm zur Determinantenberechnung Laplace Entwicklung nach 1. Spalte
+ * Ein Demo-Programm zur Determinantenberechnung Laplace Entwicklung nach 1.
+ * Spalte
+ * 
  * @author Friedhelm Schwenker
  * @version 1.0
  */
 
-
 public class Determinante {
 
     public static void main(String[] args) {
-	int n = 3; 				// Vorsicht n<=10 
-        int [][] mat = new int [n][n]; 
+        int n = 3; // Vorsicht n<=10
+        int[][] mat = new int[n][n];
 
- 	init(mat);
-	printMatrix(mat);
+        init(mat);
+        printMatrix(mat);
 
-       System.out.println("det(M) = " + det(mat));	
+        System.out.println("det(M) = " + det(mat, 0, 0));
 
     }
 
     // initialisiert mat mit zufälligen werten
-    private static void init(int [][] mat){
-	float scale = 10.0f;
-	for(int i=0; i < mat.length; i++) {
-           for(int j=0; j < mat.length; j++) {
-               mat[i][j] = (int) ((Math.random() - 0.5) * scale);
-           }
-       }
+    private static void init(int[][] mat) {
+        float scale = 10.0f;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                mat[i][j] = (int) ((Math.random() - 0.5) * scale);
+            }
+        }
     }
 
-    // implementieren sie diese methode
-    private static int det(int [][] mat){
+    // recursive determinant calculation --> does not work
+    private static int det(int[][] mat, int sum, int i) {
         // TODO
-
+        if (mat.length <= 10 && i < mat.length - 1) {
+            // sum i = 0 to n of (-1^i+1 * mi1 * det * m-i1) -1^i+1 * mi1 = a
+            sum += (Math.pow(-1, (i + 1)) * mat[i][1] * calculateDeterminanteReducedMatrix(reduceMatrix(mat, i)));
+            return det(mat, sum, (i + 1));
+        } else if (mat.length == 1) {
+            return mat[0][0];
+        } else if (i == mat.length-1) {
+            return sum;
+        } else {
+            System.out.println("Input error");
+            return -1;
+        }
 
     }
 
-    // streicht die erste sparlte und zeile row aus mat
-    private static int [][] reduceMatrix(int [][] mat, int row){
-	int [][] redMat = new int [mat.length-1][mat.length-1];
-	
-	for(int i=0; i < redMat.length; i++){ 
-	    for(int j=0; j < redMat.length; j++){
+    //works, no touchy
+    private static int calculateDeterminanteReducedMatrix(int[][] mat) {
+        if (mat[0][0] == 0 && mat[0][1] == 0 || mat[0][0] == 0 && mat[1][0] == 0
+        || mat[0][1] == 0 && mat[1][1] == 0) {
+            return 0;
+        } else {
+            return (mat[0][0] * mat[1][1]) - (mat[1][0] * mat[0][1]);
+        }
+    }
+
+    // Streicht die erste spalte und Zeile row aus mat
+    private static int[][] reduceMatrix(int[][] mat, int row) {
+        int[][] redMat = new int[mat.length - 1][mat.length - 1];
+
+        for (int i = 0; i < redMat.length; i++) {
+            for (int j = 0; j < redMat.length; j++) {
                 if (i < row)
-          	    redMat[i][j] = mat[i][j+1];
-          	else
-	            redMat[i][j] = mat[i+1][j+1];
-	    }
-	}
-	return redMat;
-    } 
+                    redMat[i][j] = mat[i][j + 1];
+                else
+                    redMat[i][j] = mat[i + 1][j + 1];
+            }
+        }
+        return redMat;
+    }
 
     // gibt den inhalt von mat aus
-    private  static void printMatrix(int [][] mat){
-        for(int i=0; i < mat.length; i++){ 
-            for(int j=0; j < mat.length; j++)
+    private static void printMatrix(int[][] mat) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++)
                 System.out.print(mat[i][j] + " ");
-                System.out.println();
-	}
+            System.out.println();
+        }
     }
 
-
-}   
+}
